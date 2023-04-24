479b5ac3cdb6a359e0415f6c30c5efc9fe91d131
pnhung177 <pnhung177@gmail.com>
2023-04-24 09:07:48 +0700

    Update the motor control functions

--- code/esp8266-cp2102-blynk/esp8266-cp2102-blynk.ino
+++ code/esp8266-cp2102-blynk/esp8266-cp2102-blynk.ino
@@ -1,5 +1,7 @@
 #define BLYNK_PRINT Serial
 
+#define ROBIGAMI_DEBUG 1
+
 #include "./credentials-blynk.h"
 #include "./credentials-wifi.h"
 
@@ -8,6 +10,124 @@
 
 char wifi_ssid[] = WIFI_SSID;
 char wifi_password[] = WIFI_PASSWORD;
+
+#define RightMotorSpeed 5
+#define RightMotorDir   0 
+#define LeftMotorSpeed  4
+#define LeftMotorDir    2
+
+// 312 = 512 - 200
+// int minRange = 312;
+// 64 = 128 - 64
+int minRange = 64;
+
+// 712 = 512 + 200
+// int maxRange = 712;
+// 292 = 128 + 64
+int maxRange = 192;
+
+int minSpeed = 450;
+int maxSpeed = 1020;
+int noSpeed = 0;
+
+
+void moveControl(int x, int y) {
+
+  // move forward
+  if(y >= maxRange && x >= minRange && x <= maxRange)
+  {
+    digitalWrite(RightMotorDir,HIGH);
+    digitalWrite(LeftMotorDir,HIGH);
+    analogWrite(RightMotorSpeed,maxSpeed);
+    analogWrite(LeftMotorSpeed,maxSpeed);
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Move forward");
+    #endif
+  }
+ 
+  // move forward right
+  else if(x >= maxRange && y >= maxRange)
+  {
+    digitalWrite(RightMotorDir,HIGH);
+    digitalWrite(LeftMotorDir,HIGH);
+    analogWrite(RightMotorSpeed,minSpeed);
+    analogWrite(LeftMotorSpeed,maxSpeed);
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Move forward right");
+    #endif
+  }
+
+  // move forward left
+  else if(x <= minRange && y >= maxRange)
+  {
+    digitalWrite(RightMotorDir,HIGH);
+    digitalWrite(LeftMotorDir,HIGH);
+    analogWrite(RightMotorSpeed,maxSpeed);
+    analogWrite(LeftMotorSpeed,minSpeed);
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Move forward left");
+    #endif
+  }
+
+  // neutral zone
+  else if(y < maxRange && y > minRange && x < maxRange && x > minRange)
+  {
+    analogWrite(RightMotorSpeed,noSpeed);
+    analogWrite(LeftMotorSpeed,noSpeed);
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Stand by");
+    #endif
+  }
+
+  // move back
+  else if(y <= minRange && x >= minRange && x <= maxRange)
+  {
+    digitalWrite(RightMotorDir,LOW);
+    digitalWrite(LeftMotorDir,LOW);
+    analogWrite(RightMotorSpeed,maxSpeed);
+    analogWrite(LeftMotorSpeed,maxSpeed);
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Move backward");
+    #endif
+  }
+
+  // move back and right
+  else if(y <= minRange && x <= minRange)
+  {
+    digitalWrite(RightMotorDir,LOW);
+    digitalWrite(LeftMotorDir,LOW);
+    analogWrite(RightMotorSpeed,minSpeed);
+    analogWrite(LeftMotorSpeed,maxSpeed); 
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Move back right");
+    #endif
+  }
+
+  // move back and left
+  else if(y <= minRange && x >= maxRange)
+  {
+    digitalWrite(RightMotorDir,LOW);
+    digitalWrite(LeftMotorDir,LOW);
+    analogWrite(RightMotorSpeed,maxSpeed);
+    analogWrite(LeftMotorSpeed,minSpeed);
+    #if defined ROBIGAMI_DEBUG
+    Serial.println("Move back left");
+    #endif
+  }
+}
+
+void initControl() { 
+  // initial settings for motors off and direction forward
+  pinMode(RightMotorSpeed, OUTPUT);
+  pinMode(LeftMotorSpeed, OUTPUT);
+  pinMode(RightMotorDir, OUTPUT);
+  pinMode(LeftMotorDir, OUTPUT);
+ 
+  digitalWrite(RightMotorSpeed, LOW);
+  digitalWrite(LeftMotorSpeed, LOW);
+  digitalWrite(RightMotorDir, HIGH);
+  digitalWrite(LeftMotorDir,HIGH);
+}
 
 // This function is called every time the device is connected to the Blynk.Cloud
 BLYNK_CONNECTED()
@@ -24,9 +144,17 @@ BLYNK_WRITE(V7)
   int y = param[1].asInt();
 
   // Do something with x and y
-  Serial.print("X7 = ");
+  logJoystickChange(7, x, y);
+
+  moveControl(x, y);
+}
+
+void logJoystickChange(int vPin, int x, int y) {
+  Serial.print("Pin: V");
+  Serial.print(vPin);
+  Serial.print("; X: ");
   Serial.print(x);
-  Serial.print("; Y7 = ");
+  Serial.print("; Y: ");
   Serial.println(y);
 }
 
@@ -36,6 +164,9 @@ void setup() {
 
   // Connect and authenticate to Blynk.cloud
   Blynk.begin(BLYNK_AUTH_TOKEN, wifi_ssid, wifi_password);
+
+  //
+  initControl();
 }
 
 void loop() {
